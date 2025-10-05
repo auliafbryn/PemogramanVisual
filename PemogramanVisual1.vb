514664f0663c4fd4735a50e06e907304d6cb0ecd
Aulia Febriyani <fbrynaulia@gmail.com>
2025-10-05 17:03:01 +0700

    Add files via upload

---
+++ PemogramanVisual1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        ' --- Input Data dari User ---
+        Dim nama As String
+        Dim prodi As String
+        Dim kampus As String
+        Dim hobi As String
+
+        Console.Write("Masukkan Nama: ")
+        nama = Console.ReadLine()
+
+        Console.Write("Masukkan Program Studi: ")
+        prodi = Console.ReadLine()
+
+        Console.Write("Masukkan Nama Kampus: ")
+        kampus = Console.ReadLine()
+
+        Console.Write("Masukkan Hobi: ")
+        hobi = Console.ReadLine()
+
+        ' --- Output Hasil ---
+        Console.WriteLine()
+        Console.WriteLine("Halo, nama saya adalah " & nama)
+        Console.WriteLine("Saya kuliah pada jurusan " & prodi & " di " & kampus)
+        Console.WriteLine("Saya memiliki hobi " & hobi)
+
+        Console.WriteLine()
+        Console.WriteLine("Tekan tombol apa saja untuk keluar...")
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ PemogramanVisual2.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        ' --- Input Bilangan ---
+        Dim a As Integer
+        Dim b As Integer
+
+        Console.Write("Masukkan bilangan pertama: ")
+        a = Convert.ToInt32(Console.ReadLine())
+
+        Console.Write("Masukkan bilangan kedua: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        Console.WriteLine()
+
+        ' --- Operasi Penjumlahan ---
+        Dim tambah As Integer = a + b
+        If tambah < 0 Then tambah = 0
+        Console.WriteLine("Hasil Penjumlahan: " & tambah)
+
+        ' --- Operasi Perkalian ---
+        Dim kali As Integer = a * b
+        If kali < 0 Then kali = 0
+        Console.WriteLine("Hasil Perkalian: " & kali)
+
+        ' --- Operasi Pembagian ---
+        Dim bagi As Integer
+        If b <> 0 Then
+            bagi = a \ b   ' pembagian bulat
+            If bagi < 0 Then bagi = 0
+        Else
+            bagi = 0
+        End If
+        Console.WriteLine("Hasil Pembagian: " & bagi)
+
+        ' --- Operasi Modulus ---
+        Dim modVal As Integer = 0 ' langsung ditetapkan 0
+        Console.WriteLine("Hasil Modulus: " & modVal)
+
+        Console.WriteLine()
+        Console.WriteLine("Tekan tombol apa saja untuk keluar...")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
